22422d63c9d3fa9ef9841f21a2ac96b06d747458
Faisal Khan <faisalkhan.chat@gmail.com>
2017-09-07 13:05:41 +0530

    footer design changes done
    new updates done

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+ $(document).ready(function() {
+        $('.owl-testimonial').owlCarousel({   
+            animateOut: 'slideOutDown',
+            animateIn: 'flipInX',
+            items:1,
+            margin:0,
+            stagePadding:0,
+            smartSpeed:450
+        });
+    });
